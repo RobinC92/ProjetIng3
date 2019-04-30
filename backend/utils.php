73c869908223873046fe9182e3a3a370cdff612e
hugodesgouillon <hugo.desgouillon@edu.ece.fr>
2019-04-30 12:46:30 +0200

    connexion

--- backend/utils.php
+++ backend/utils.php
@@ -1,19 +1,22 @@
 <?php
     $nom = isset($_POST["nom"])? $_POST["nom"] : "";
     $prenom = isset($_POST["prenom"])? $_POST["prenom"] : "";
+    $pseudo = isset($_POST["pseudo"])? $_POST["pseudo"] : "";
     $statut = isset($_POST["statut"])? $_POST["statut"] : "";
     $mail = isset($_POST["mail"])? $_POST["mail"] : "";
     $mdp = isset($_POST["mdp"])? $_POST["mdp"] : "";
     $adresse = isset($_POST["adresse"])? $_POST["adresse"] : "";
-    //identifier votre BDD
-    $database = "livres";
+    $photo = isset($_POST["photo"])? $_POST["photo"] : "";
+
+    // identifier votre BDD
+    $database = "amazon";
     //connectez-vous dans votre BDD
     //Rappel: votre serveur = localhost et votre login = root et votre password = <rien>
     $db_handle = mysqli_connect('localhost', 'root', '');
     $db_found = mysqli_select_db($db_handle, $database);
-    if ($_POST["button2"]) {
-        $sql = "INSERT INTO books(Titre, Auteur, Annee, Editeur)
-        VALUES('$titre', '$auteur', '$annee', '$editeur')";
+    if ($_POST["button"]) {
+        $sql = "INSERT INTO individu VALUES('$nom', '$prenom', '$pseudo', '$statut', '$mail', '$mdp', '$adresse','$photo')";
         $result = mysqli_query($db_handle, $sql);
+        echo "ca a marché !!";
     }
 ?>
--- premiere_connexion.php
+++ premiere_connexion.php
@@ -6,37 +6,41 @@
 </head>
 <body>
 	<h2>Création d'un compte</h2>
-	<form>
+	<form action="utils.php" method="post">
 		<table>
 			<tr><td>Nom:</td>
-				<td><input type="text" name="nom"></td>
+				<td><input type="text" name="nom" required="true"></td>
 			</tr>
 			<tr>
 				<td>Prénom:</td>
-				<td><input type="text" name="prenom"></td>
+				<td><input type="text" name="prenom" required="true"></td>
+			</tr>
+			<tr>
+				<td>Pseudo:</td>
+				<td><input type="text" name="pseudo" required="true"></td>
 			</tr>
 			<tr>
 				<td>Statut:</td>
-				<td><select name="classe" id="classe">
+				<td><select name="statut" id="classe">
 					<option value="acheteur">Acheteur</option>
 					<option value="vendeur">Vendeur</option>
 				</select></td>
 			</tr>
 			<tr>
 				<td>Email:</td>
-				<td><input type="text" name="mail"></td>
+				<td><input type="text" name="mail" required="true"></td>
 			</tr>
 			<tr>
 				<td>Mot de passe:</td>
-				<td><input type="password" name="mdp"></td>
+				<td><input type="password" name="mdp" required="true"></td>
 			</tr>
 			<tr>
 				<td>Adresse:</td>
-				<td><input type="text" name="adresse"></td>
+				<td><input type="text" name="adresse" required="true"></td>
 			</tr>
 			<tr>
 				<td>Photo de profil:</td>
-				<td><input type="file" name="photo"></td>
+				<td><input type="file" name="photo" required="true"></td>
 			</tr>
 			<tr>
 				<td colspan="2" align="center">
