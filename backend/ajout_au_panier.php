db482089ada9c00994499edd1bbb8ebdf84ba606
RobinC92 <robin@canaguier.com>
2019-05-01 17:42:12 +0200

    AjouterPanier fin

--- backend/ajout_au_panier.php
+++ backend/ajout_au_panier.php
@@ -2,15 +2,24 @@
     require 'includes/connect_db.php';
 
     function ajouterAuPanier($id){
+        session_start();
+        $pseudo = $_SESSION['pseudo'];
         if ($db_found) {
-            $sql = "SELECT * FROM individu";
+            $sql = "SELECT * FROM individu WHERE Pseudo LIKE '%$pseudo%'";    
             $result = mysqli_query($db_handle, $sql);
-            if(){
-                $array = array($id);
-                $envoyer = serialize($array);
+            $data = mysqli_fetch_assoc($result);
+            if($data['Pseudo'] == NULL){
+                $_SESSION['pseudo'] = array($id);
+                $envoyer = serialize($_SESSION['pseudo']);
                 $sql = "INSERT INTO individu(Panier)
-                $pseudo = $_SESSION['pseudo']
-                VALUES('$envpyer')
+                VALUES('$envoyer')
+                WHERE Pseudo LIKE '%$pseudo%'";
+            }
+            else{
+                array_push($_SESSION['pannier'],$id);
+                $envoyer = serialize($_SESSION['pseudo']);
+                $sql = "INSERT INTO individu(Panier)
+                VALUES('$envoyer')
                 WHERE Pseudo LIKE '%$pseudo%'";
             }
         }
