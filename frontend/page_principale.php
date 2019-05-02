a947f646870787019ad78d7a1c66e47b4c96eced
hugodesgouillon <hugo.desgouillon@edu.ece.fr>
2019-05-02 23:32:19 +0200

    .

--- frontend/page_principale.php
+++ frontend/page_principale.php
@@ -65,6 +65,10 @@
 
 <style type="text/css">
 	
+body{
+  background-color:GhostWhite;
+}
+
 .dropdown{
 	margin-top: 8px;
 }
@@ -75,7 +79,8 @@
   bottom: 0;
   left: 0;
   padding: 1rem;
-  background-color: #efefef;
+  background-color: DimGray;
+  color: white;
   text-align: center;
 }
 
