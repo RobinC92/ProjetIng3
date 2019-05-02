71dcb786ab7954310934fc3db71356d1273fa6cc
RobinC92 <robin@canaguier.com>
2019-05-02 12:22:11 +0200

    VérificationPannier

--- backend/panier.php
+++ backend/panier.php
@@ -1,51 +1,57 @@
 <?php   
-    require 'includes/connect_db.php';
-
+    
     function ajouterAuPanier($id){
-        session_start();
+        //session_start();
+        require 'includes/connect_db.php';
         $pseudo = $_SESSION['pseudo'];
         if ($db_found) {
             $sql = "SELECT * FROM individu WHERE Pseudo LIKE '%$pseudo%'";    
             $result = mysqli_query($db_handle, $sql);
             $data = mysqli_fetch_assoc($result);
             if($data['Panier'] == NULL){
+                echo "Option 1 <br>";
                 $_SESSION['panier'] = array($id);
-                $envoyer = serialize($_SESSION['panier']);
-                $sql = "INSERT INTO individu(Panier)
-                VALUES('$envoyer')
-                WHERE Pseudo LIKE '%$pseudo%'";
-            }
-            else{
-                array_push($_SESSION['pannier'],$id);
                 $envoyer = serialize($_SESSION['panier']);
                 $sql = "UPDATE individu
                 SET Panier = '$envoyer'
                 WHERE Pseudo LIKE '%$pseudo%'";
+                $result = mysqli_query($db_handle, $sql);
+            }
+            else{
+                array_push($_SESSION['panier'],$id);
+                $envoyer = serialize($_SESSION['panier']);
+                $sql = "UPDATE individu
+                SET Panier = '$envoyer'
+                WHERE Pseudo LIKE '%$pseudo%'";
+                $result = mysqli_query($db_handle, $sql);
             }
         }
     }
 
     function supprimerDuPanier($id){
-        session_start();
+        require 'includes/connect_db.php';
+        //session_start();
         $pseudo = $_SESSION['pseudo'];
-        unset($_SESSION['pannier'][array_search($id, $_SESSION['pannier'])]);
+        unset($_SESSION['panier'][array_search($id, $_SESSION['panier'])]);
         $envoyer = serialize($_SESSION['panier']);
         if ($db_found) {
-            $envoyer = serialize($_SESSION['pseudo']);
+            $envoyer = serialize($_SESSION['panier']);
             $sql = "UPDATE individu
             SET Panier = '$envoyer'
             WHERE Pseudo LIKE '%$pseudo%'";
+            $result = mysqli_query($db_handle, $sql);
         }
     }
 
     function totalAPayer(){
-        session_start();
+        //session_start();
+        require 'includes/connect_db.php';
         $total = 0;
         $sql = "SELECT * FROM produit";
         $result = mysqli_query($db_handle, $sql);
         while ($db_field = mysqli_fetch_assoc($result)) {
             foreach($_SESSION['panier'] as $value){
-                if($value == $db_field['id']){
+                if($value == $db_field['ID']){
                     $total = $total + $db_field['Prix'];
                 }
             }
@@ -58,7 +64,7 @@
         $result = mysqli_query($db_handle, $sql);
         while ($db_field = mysqli_fetch_assoc($result)) {
             foreach($_SESSION['panier'] as $value){
-                if($value == $db_field['id']){
+                if($value == $db_field['ID']){
                     $sql = "SELECT * FROM individu";
                     $result2 = mysqli_query($db_handle, $sql2);
                     while ($db_field2 = mysqli_fetch_assoc($result2)) {
