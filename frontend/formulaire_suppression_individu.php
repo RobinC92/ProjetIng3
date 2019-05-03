9d6a1e86bdd65e75bfe13e278c9201b9c9733b48
julienrauber <julien.rauber@edu.ece.fr>
2019-05-03 19:47:23 +0200

    formulaire suppression individu css

--- frontend/formulaire_suppression_individu.php
+++ frontend/formulaire_suppression_individu.php
@@ -1,26 +1,28 @@
 <?php
-    require_once('page_principale.php');
+require_once('page_principale.php');
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>amazon</title>
+	<link rel='stylesheet' type='text/css' media='screen' href='formulaire.css'>
 </head>
-<body>
-	<h2> Quel compte voulez-vous supprimer ?</h2>
-	<form action="../backend/suppression_individu" method="post">
-		<table>
-			<tr>
-				<td>pseudo du compte:</td>
-				<td><input type="text" name="pseudo" required="true"></td>
-			</tr>
-			</tr>
-				<td colspan="2" align="center">
-				<input type="submit" name="button" value="Suppression"></td>
-			</tr>
-		</table>
-	</form>
 
+<body>
+
+	<div class="form-style-2">
+		<div class="form-style-2-heading">Quel compte voulez-vous suprimmer</div>
+		<br><br><br>
+		<form action="../backend/suppression_individu" method="post" enctype="multipart/form-data">
+
+			<label for="field1"><span>Pseudo du compte <span class="required">*</span></span><input required="true" type="text" class="input-field" name="pseudo" value="" /></label>
+
+			<label><span> </span><input type="submit" name="button" value="Suppression" /></label>
+
+		</form>
+	</div>
 </body>
+
 </html>
