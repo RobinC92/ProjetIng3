53f1b98abd610cc7bcca79ac4818d28cd86cf87f
RobinC92 <robin@canaguier.com>
2019-05-03 15:37:36 +0200

    Panier

--- frontend/affichage_produits.php
+++ frontend/affichage_produits.php
@@ -1,5 +1,7 @@
+
 <?php
     function affichageProduits($produits){
+        session_start();
         for ($i=0;$i<count($produits);$i++){
             echo"
             <div class='ville'>
@@ -8,8 +10,20 @@
                 <p>".$produits[$i]->prix."</p>
                 <p>".$produits[$i]->categorie."</p>
                 <p>".$produits[$i]->description."</p>
-                <center><a href='#'>Ajouter au Panier</a></center>
+                <center><a href='../backend/ajoutAuPanier.php?myid=".$produits[$i]->id."'>Ajouter au Panier</a></center>
             </div>";
         }
+    }
+
+    function affichageProduit($produits){
+        echo"
+        <div class='ville'>
+            <a href='#'><img src='"."../backend/".$produits->photo."'height='250'></a>
+            <h2>".$produits->nom."</h2>
+            <p>".$produits->prix."</p>
+            <p>".$produits->categorie."</p>
+            <p>".$produits->description."</p>
+            <center><a href='../backend/supprimerDuPanier.php?myid=".$produits->id."'>Supprimer du Panier</a></center>
+        </div>";
     }
 ?>
